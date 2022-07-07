29855752120d7958f0ece05c67889c6401ecf418
Alan Chiu <ms0372631@gmail.com>
2022-07-06 17:58:05 -0700

    dd

---
+++ src/App.js
@@ -0,0 +1,22 @@
+// functional vs class
+// state and lifecycle
+
+export default function App() {
+  return (
+    <div className="App">
+      {/* <div>section 1. useState and useEffect</div>
+      <BasicHooksDemo />
+      <hr /> */}
+      {/* <hr />
+      <div>section 2. useRef</div>
+      <Ref /> */}
+      {/* <div>section 3. useMemo</div>
+      <Memo />
+      <hr /> */}
+      {/* <div>section 4. useCallback</div>
+      <Callback />
+      <hr /> */}
+    
+    </div>
+  );
+}
